42f21c16a4b4f864d44daec349f0d0fabd1c94e4
park-siwan <siwandevelop@gmail.com>
2022-06-12 20:08:11 +0900

    Layout 블로그 구성

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,5 @@
 import '../styles/global.css';
 
 export default function App({ Component, pageProps }) {
-  return (
-    <div>
-      {/* <header>
-        <h1>GNB 헤더</h1>
-      </header> */}
-      <Component {...pageProps} />
-      {/* <footer>
-        <h2>푸터</h2>
-      </footer> */}
-    </div>
-  );
+  return <Component {...pageProps} />;
 }
