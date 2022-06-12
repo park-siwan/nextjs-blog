4f91ab2dc893955d25555ee65216153bfcc31ee8
park-siwan <siwandevelop@gmail.com>
2022-06-12 13:30:54 +0900

    global css 적용

---
+++ pages/_app.js
@@ -0,0 +1,15 @@
+import '../styles/global.css';
+
+export default function App({ Component, pageProps }) {
+  return (
+    <div>
+      <header>
+        <h1>GNB 헤더</h1>
+      </header>
+      <Component {...pageProps} />
+      <footer>
+        <h2>푸터</h2>
+      </footer>
+    </div>
+  );
+}
